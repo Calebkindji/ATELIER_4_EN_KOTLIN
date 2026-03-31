d81e4a1992343ec4fe676e65a9f0076d70a79c44
Calebkindji <23mk324si@esisalama.org>
2026-03-31 12:38:02 +0100

    Ajout de la page d'acceuil

--- app/src/main/java/com/example/tp_affirmation/MainActivity.kt
+++ app/src/main/java/com/example/tp_affirmation/MainActivity.kt
@@ -9,12 +9,16 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -22,6 +26,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -39,11 +45,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.tp_affirmation.data.DataSource
 import com.example.tp_affirmation.model.Country
@@ -229,25 +239,18 @@ fun CountryList(
     }
 }
 
-@Composable
-fun CountryApp() {
-    Scaffold(
-        topBar = {
-            AffirmationTopAppBar()
-        }
-    ) { innerPadding ->
-        CountryList(
-            countryList = DataSource().loadCountries(),
-            contentPadding = innerPadding,
-            modifier = Modifier.fillMaxSize()
-        )
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun CountryPreview() {
     TP_AffirmationTheme {
         CountryApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    TP_AffirmationTheme {
+        HomeScreen(onStartClicked = {})
     }
 }
